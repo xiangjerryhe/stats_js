f3b72cf8ee200aff92f0543d8b211678fc300f5f
xiangjerryhe <89714881+xiangjerryhe@users.noreply.github.com>
2021-09-29 00:24:38 -0400

    Create pca.js
    PCA implemented as autocoder with linear transfer function because alas Google has not yet ported tf.linalg.svd to javascript

---
+++ pca.js
@@ -0,0 +1,25 @@
+// Principle Component Analysis implementation in javascript using tensorflow.js
+import * as tf from "@tensorflow/tfjs";
+
+class PCA {
+  constructor(newdim, orig_dim) {
+       this.encoder = tf.layers.dense({
+                         units: newdim,
+                         batchInputShape:[null, orig_dim],
+                         kernelInitializer:"randomNormal",
+                         biasInitializer:"zeros",
+                         activation: 'relu',
+                       })
+       this.decoder = tf.layers.dense({units:orig_dim, activation:'relu'})
+       this.model = tf.sequential({
+             layers:[ this.encoder ,  this.decoder] })
+        this.model.compile({optimizer:'sgd', loss:'meanSquaredError'})
+  }
+  
+  fit(X) {
+         this.model.fit(X, X, {epochs:80, shuffle:true, validationSplit:0.3)  
+  }
+  transform(X) {
+        return this.encoder.predict(X)          
+  }
+}
